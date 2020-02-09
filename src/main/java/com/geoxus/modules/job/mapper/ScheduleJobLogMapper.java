dc52d09351c4479f348f9c39f7d0b7c821c4d02f
枫叶思源 <britton@126.com>
2020-02-09 10:23:21 +0800

    模块重构

--- src/main/java/com/geoxus/modules/job/mapper/ScheduleJobLogMapper.java
+++
@@ -1,9 +0,0 @@
-package com.geoxus.modules.job.mapper;
-
-import com.geoxus.core.common.mapper.GXBaseMapper;
-import com.geoxus.modules.job.entity.ScheduleJobLogEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ScheduleJobLogMapper extends GXBaseMapper<ScheduleJobLogEntity> {
-}
