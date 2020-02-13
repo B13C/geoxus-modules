17a47b02afe146c10a50f8408a166dac0bcac217
枫叶思源 <britton@126.com>
2020-02-13 14:48:54 +0800

    新增限制一段时间的服务次数限制功能

--- src/main/java/com/geoxus/modules/test/controller/frontend/TestController.java
+++ src/main/java/com/geoxus/modules/test/controller/frontend/TestController.java
@@ -1,14 +1,11 @@
 package com.geoxus.modules.test.controller.frontend;
 
 import cn.hutool.core.lang.Dict;
-import com.geoxus.core.common.annotation.GXLoginAnnotation;
-import com.geoxus.core.common.annotation.GXLoginUserAnnotation;
+import com.geoxus.core.common.annotation.GXDurationCountLimitAnnotation;
 import com.geoxus.core.common.controller.GXController;
-import com.geoxus.core.common.util.GXCommonUtils;
 import com.geoxus.core.common.util.GXResultUtils;
 import com.geoxus.core.rpc.service.GXRabbitMQRPCClientService;
 import com.geoxus.modules.test.entity.TestEntity;
-import com.geoxus.modules.user.entity.UUserEntity;
 import com.geoxus.modules.user.service.UUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,10 +23,8 @@ public class TestController implements GXController<TestEntity> {
     private UUserService uUserService;
 
     @PostMapping("/test")
-    @GXLoginAnnotation
-    public GXResultUtils test(@RequestBody Dict param, @GXLoginUserAnnotation UUserEntity uUserEntity) {
-        final String queueName = GXCommonUtils.getRemoteRPCServerValueByKey("abiz-server", "server-name");
-        final Dict call = rabbitMQRPCClientService.call("testMethod", "btTestServerHandler", param, queueName);
-        return GXResultUtils.ok().putData(call);
+    @GXDurationCountLimitAnnotation(key = "hello")
+    public GXResultUtils test(@RequestBody Dict param) {
+        return GXResultUtils.ok();
     }
 }
