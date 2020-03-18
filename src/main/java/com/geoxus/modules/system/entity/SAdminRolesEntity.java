d9b9aaf6469549c2cc94006a979f39b76c296300
枫叶思源 <britton@126.com>
2020-03-18 15:37:12 +0800

    按照sonar的建议完善代码

--- src/main/java/com/geoxus/modules/system/entity/SAdminRolesEntity.java
+++ src/main/java/com/geoxus/modules/system/entity/SAdminRolesEntity.java
@@ -2,7 +2,7 @@ package com.geoxus.modules.system.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.geoxus.core.common.entity.GXSAdminHasRolesEntity;
+import com.geoxus.core.common.entity.GXSAdminRolesEntity;
 import com.geoxus.modules.system.constant.SAdminRolesConstants;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -15,7 +15,7 @@ import java.io.Serializable;
 @Data
 @TableName(SAdminRolesConstants.TABLE_NAME)
 @EqualsAndHashCode(callSuper = false)
-public class SAdminRolesEntity extends GXSAdminHasRolesEntity implements Serializable {
+public class SAdminRolesEntity extends GXSAdminRolesEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @TableId
