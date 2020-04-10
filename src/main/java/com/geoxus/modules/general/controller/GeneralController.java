646b3d4dcb540a122221280cb109ab1d9a69fd50
枫叶思源 <britton@126.com>
2020-04-10 14:40:55 +0800

    新增获取所有核心模型的方法

--- src/main/java/com/geoxus/modules/general/controller/GeneralController.java
+++ src/main/java/com/geoxus/modules/general/controller/GeneralController.java
@@ -1,16 +1,20 @@
 package com.geoxus.modules.general.controller;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.lang.Dict;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.geoxus.core.common.annotation.GXLoginAnnotation;
 import com.geoxus.core.common.annotation.GXLoginUserAnnotation;
 import com.geoxus.core.common.service.GXApiIdempotentService;
 import com.geoxus.core.common.util.GXResultUtils;
+import com.geoxus.core.framework.entity.GXCoreModelEntity;
 import com.geoxus.core.framework.service.GXCoreModelAttributesService;
 import com.geoxus.core.framework.service.GXCoreModelService;
 import com.geoxus.user.entity.UUserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -35,5 +39,18 @@ public class GeneralController {
     public GXResultUtils getModelAttributes(@RequestBody Dict param) {
         final List<Dict> attributes = gxCoreModelAttributesService.getModelAttributesByModelId(param);
         return GXResultUtils.ok().putData(attributes);
+    }
+
+    @PostMapping("/get-all-models")
+    public GXResultUtils getAllModels() {
+        final ArrayList<Dict> data = CollUtil.newArrayList();
+        final List<GXCoreModelEntity> list = coreModelService.list(new QueryWrapper<GXCoreModelEntity>().select("model_id", "model_show"));
+        if (null != list && !list.isEmpty()) {
+            for (GXCoreModelEntity entity : list) {
+                final Dict set = Dict.create().set("model_id", entity.getModelId()).set("model_show", entity.getModelShow());
+                data.add(set);
+            }
+        }
+        return GXResultUtils.ok().putData(data);
     }
 }
